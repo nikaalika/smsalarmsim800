e73f4f689deac9d945f6fac386e95c763c9a2063
nikaalika <53902741+nikaalika@users.noreply.github.com>
2019-10-08 12:23:44 +0400

    Update new.ino

--- new.ino
+++ new.ino
@@ -2,9 +2,9 @@
 SoftwareSerial sim(8, 9); 
 int _timeout;
 String _buffer;
-const int buttonPin = 5;          
+const int buttonPin = 5;    //digital input      
 int previousButtonState = HIGH;   
-String num= "+995599516463";
+String num= "yourphonenumber";
 
 void setup() {
   Serial.begin(9600);              
@@ -16,16 +16,16 @@ void setup() {
   if ((buttonState != previousButtonState)
       && (buttonState == HIGH)) 
                 {
-  String SMS = "kari gaigo";
-  sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  String SMS = "yourtext";
+  sim.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode 
   delay(1000);
    
-  sim.println("AT+CMGS=\"" + num + "\"\r");
+  sim.println("AT+CMGS=\"" + num + "\"\r"); //inputs phone number
   delay(2000);
   sim.println(SMS);
   delay(2000);
   
-  sim.println((char)26);// ASCII code of CTRL+Z
+  sim.println((char)26);// ASCII code of CTRL+Z,initializez sms send 
   delay(9000);       
   }
   previousButtonState = buttonState;
